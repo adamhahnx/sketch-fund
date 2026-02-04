055f0e2e524b84f6172670fdd1e9b0e4e59479dc
adamhahnx <48037347+adamhahnx@users.noreply.github.com>
2026-02-03 20:16:09 -0700

    mural integration undeployed

--- routes/merchants.js
+++ routes/merchants.js
@@ -12,6 +12,80 @@ module.exports = function (server, opts, done) {
 			}
 		})
 
+		const counterparty = await fetch('https://api-staging.muralpay.com/api/counterparties',
+		{
+			method: 'POST',
+			headers: {
+				'Authorization': 'Bearer ' + process.env.MURAL_API_KEY,
+				accept: 'application/json',
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				counterparty: {
+					type: 'business',
+					name: merchant.name,
+					email: 'user@example.com',
+					physicalAddress: {
+						address1: '1234 Apple St',
+						address2: 'Unit A',
+						country: 'US',
+						subDivision: 'CO',
+						city: 'Denver',
+						postalCode: '80204'
+					}
+				}
+			})
+		})
+
+		let payoutMethodBody = ''
+		if (merchant.currency == 'USDC')
+		{
+			payoutMethodBody = JSON.stringify({
+				payoutMethod: {
+					type: 'usd',
+					details: {
+						type: 'usdDomestic',
+						symbol: 'USD',
+						accountType: 'CHECKING',
+						transferType: 'ACH',
+						bankAccountNumber: request.body.routingNumber,
+						bankRoutingNumber: request.body.routingNumber,
+						bankName: request.body.bankName,
+					}
+				}
+			})
+		}
+		else if (merchant.currency == 'COP')
+		{
+			payoutMethodBody = JSON.stringify({
+				payoutMethod: {
+					type: 'cop',
+					details: {
+						type: 'copDomestic',
+						symbol: 'COP',
+						phoneNumber: request.body.phoneNumber,
+						accountType: 'CHECKING',
+						documentType: 'NATIONAL_ID',
+						bankAccountNumber: request.body.routingNumber,
+						bankRoutingNumber: request.body.routingNumber,
+						transferType: 'ACH',
+						bankName: request.body.bankName,
+					}
+				}
+			})
+		}
+
+		const payoutMethod = await fetch('https://api-staging.muralpay.com/api/counterparties/' + counterparty.id + '/payout-methods',
+		{
+			method: 'POST',
+			headers: {
+				'Authorization': 'Bearer ' + process.env.MURAL_API_KEY,
+				accept: 'application/json',
+				'content-type': 'application/json'
+			},
+			body: payoutMethodBody
+		})
+
 		return response.code(201).send(merchant)
 	})
 
